df4e2b605622f66719d037dce20e3e12c029bb5a
Igor Medvedev <vinnepooh55@gmail.com>
2024-05-15 18:28:29 +0600

    added openErrorPage function

--- src/helpers.php
+++ src/helpers.php
@@ -167,7 +167,7 @@ function showAvatar (string|null $path, int $size): void {
 
 function openErrorPage(int $code = 404, string $errorMessage = "404 - Not Found"): void {
     http_response_code($code);
-    $_SESSION["message"] = $errorMessage;
+    $_SESSION["response"]["message"] = $errorMessage;
     header("Content-type: text/html; charset=UTF-8");
     include_once __DIR__ . "/../../pages/error-page.php";
 }
